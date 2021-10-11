1d2f469ae3f38b1eeb87d7e936930bc705cba9db
plamadol-cmd <62473200+plamadol-cmd@users.noreply.github.com>
2021-10-11 23:19:56 +0300

    Update RadixSort.java

--- Radix_Sort/src/RadixSort.java
+++ Radix_Sort/src/RadixSort.java
@@ -1,13 +1,15 @@
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Deque;
+import java.util.Iterator;
 import java.util.LinkedList;
 
-public class RadixSort {
-	static int multOf10 = 1;
-	static ArrayList<Integer> finalArr = new ArrayList<Integer>();
+public class Radix_Sort {
+	private int multOf10 = 1;
+	private Deque<Integer> finalArr = new LinkedList<Integer>();
+	private Iterator<Integer> listIt;
+	private Integer o1;
 	
-	public static int findMaxDigits(ArrayList<Integer> arr) {
+	//find max and then return the number of digits of max
+	public int findMaxDigits(LinkedList<Integer> arr) {
 		int max = arr.get(0);
 		int counter = 0;
 		
@@ -23,53 +25,49 @@ public class RadixSort {
 		return counter;
 	}
 	
-	public static ArrayList<Integer> extractNums(ArrayList<Integer> arr) {
-		ArrayList<Integer> preSortedArr1 = new ArrayList<Integer>();
+	//divide each number by multiple of 10: 10^0, then 10^1, then 10^2 etc
+	//if the number / multiple of 10 == 0, extract the number
+	//each iteration is going to extract the smallest numbers
+	public LinkedList<Integer> extractNums(LinkedList<Integer> arr) {
+		LinkedList<Integer> preSortedArr1 = new LinkedList<Integer>();
+		this.listIt = arr.iterator();
 		
-		for (int j = 0; j < arr.size(); j++) {
-			if (arr.get(j) / multOf10 == 0) {
-				//System.out.println("rem " + arr.get(j));
-				finalArr.add(arr.get(j));
+		while(this.listIt.hasNext()) {
+			o1 = this.listIt.next();
+			if (o1 / multOf10 == 0) {
+				//System.out.println("rem " + o2);
+				this.finalArr.offer(o1);
 			} else {
-				preSortedArr1.add(arr.get(j));
+				preSortedArr1.add(o1);
 			}
 		}
-		//System.out.println("pre" + preSortedArr1);
+		
 		return preSortedArr1;
 	}
 	
-	public static ArrayList<Integer> sortNums(ArrayList<Integer> arr) {
-		ArrayList<Integer> preSortedArr1 = new ArrayList<Integer>();
-		ArrayList<Integer> preSortedArr2 = new ArrayList<Integer>();
+	public LinkedList<Integer> sortNums(LinkedList<Integer> arr) {
+		//remove smallest numbers
+		LinkedList<Integer> preSortedArr1 = extractNums(arr);
+		//store the remaining numbers
+		LinkedList<Integer> preSortedArr2 = new LinkedList<Integer>();
 		
-		preSortedArr1 = extractNums(arr);
-		
+		//k helps sort the numbers by their last digit, from 0 to 9
 		for (int k = 0; k < 10; k++) {
-			for (int j = 0; j < preSortedArr1.size(); j++) {
-				if ((preSortedArr1.get(j) / multOf10) % 10 == k) {
-					preSortedArr2.add(preSortedArr1.get(j));
-					//System.out.println(k + " " + preSortedArr2.get(j));
+			this.listIt = preSortedArr1.iterator();
+			while(this.listIt.hasNext()) {
+				o1 = this.listIt.next();
+				//System.out.println(o1);
+				if ((o1 / this.multOf10) % 10 == k) {
+					preSortedArr2.add(o1);
+					//System.out.println(k + " " + o1);
 				}
 			}
 		}
-		multOf10 *= 10;
+		this.multOf10 *= 10;
 		return preSortedArr2;
 	}
 	
-	public static void main(String[] args) {
-		ArrayList<Integer> arr = new ArrayList<Integer>(Arrays.asList
-				(1000, 4, 25, 319, 88, 51, 3430, 8471, 701, 1, 2989, 657, 713, 10, 0));
-		int maxDigits = findMaxDigits(arr);
-		
-		for (int i = 0; i < maxDigits; i++) {
-			arr = sortNums(arr);
-			System.out.println(i + 1 + " digit from last\npresorted array: " + arr);
-			System.out.println("final array: " + finalArr);
-			System.out.println();
-		}
-		
-		finalArr.addAll(arr);
-		System.out.println("result: " + finalArr);
+	public Deque<Integer> getFinalArr() {
+		return this.finalArr;
 	}
-
 }
